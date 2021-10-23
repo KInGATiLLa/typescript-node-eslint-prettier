a61b18e702484f0e6e28365869d1865083e32a2f
KInGATiLLa <kh.galt53@gmail.com>
2021-10-23 21:25:41 +0800

    added prettier with eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   //   parserOptions:
   rules: {
+    'prettier/prettier': ['error', { usePrettierrc: true }],
     '@typescript-eslint/no-unused-vars': 'off',
     semi: ['error', 'always'],
     quotes: ['warn', 'single'],
--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import * as fs from 'fs';
+import express, { Request, Response } from 'express';
 
-function mane(num: number) {
-  return num * 12;
+function mane(num: number | undefined) {
+  return num ? num * 12 : 0;
 }
 
 if (!fs.existsSync('./img')) {
   fs.mkdirSync('./img');
 }
 
+console.log('mane -> ', mane(9));
+
 console.log('hello');
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.end('Hello world');
+});
+
+app.listen(4000, () => {
+  console.log('Running on Port :>> ', 4000);
+});
