e160c9bfc36c0ccc3432fb77f4af7455de7a6832
KInGATiLLa <kh.galt53@gmail.com>
2021-10-24 23:51:29 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      files: ['**/*.ts', '**/*.tsx'], // Your TypeScript files extension
       parserOptions: {
         project: ['./tsconfig.json'], // Specify it only for TypeScript files
       },
